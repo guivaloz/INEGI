1ca01dda6c9d04c383a9e0c3cc9847dfd85b1ba8
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-09-18 08:31:44 -0500

    Nueva vista en DENUE.

--- DENUE/vistas-eliminar.sql
+++ DENUE/vistas-eliminar.sql
@@ -6,4 +6,5 @@ DROP VIEW IF EXISTS den_denue_centro_historico;
 
 DROP VIEW IF EXISTS den_denue_comercio_al_por_menor;
 DROP VIEW IF EXISTS den_denue_servicios_medicos;
+DROP VIEW IF EXISTS den_denue_servicios_financieros_y_de_seguros;
 DROP VIEW IF EXISTS den_denue_servicios_preparacion_alimentos_bebidas;
--- DENUE/vistas.sql
+++ DENUE/vistas.sql
@@ -1,6 +1,10 @@
 --
 -- vistas.sql
 --
+-- sector
+--   subsector
+--     rama
+--       subrama
 
 -- Conglomerado 'Centro Histórico'
 
@@ -54,6 +58,32 @@ CREATE VIEW den_denue_comercio_al_por_menor AS
         AND ss.sector   = s.id;
 
 GRANT SELECT ON den_denue_comercio_al_por_menor TO inegi;
+
+-- 52 Servicios Financieros y de Seguros
+
+DROP VIEW IF EXISTS den_denue_servicios_financieros_y_de_seguros;
+
+CREATE VIEW den_denue_servicios_financieros_y_de_seguros AS
+    SELECT
+        d.id, d.corte, r.titulo AS rama, sr.titulo AS subrama,
+        d.nombre, d.razon_social, d.codigo, d.personal_ocupado,
+        d.calle, d.numero_ext, d.letra_ext, d.numero_int, d.letra_int, d.cp,
+        d.entidad, d.municipio, d.localidad, d.ageb, d.manzana,
+        d.coordenadas
+    FROM
+        den_denue         d,
+        scian_subramas    sr,
+        scian_ramas       r,
+        scian_subsectores ss,
+        scian_sectores    s
+    WHERE
+        s.codigo        = '52'
+        AND d.subrama   = sr.id
+        AND sr.rama     = r.id
+        AND r.subsector = ss.id
+        AND ss.sector   = s.id;
+
+GRANT SELECT ON den_denue_servicios_financieros_y_de_seguros TO inegi;
 
 -- 621 Servicios médicos de consulta externa y servicios relacionados
 
